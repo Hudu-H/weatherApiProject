1185568116c94af3ae49dd816a404667268dba56
Hudu Hamed <johndoe@example.com>
2022-12-07 21:29:56 +0100

    cleaned-up code base

--- wea.js
+++ wea.js
@@ -1,62 +1,70 @@
 let weather = {
-    'apiKey':'1365c68e3f688236f5e083df3a316a78',
-    fetchWeather: function(city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
-        + city 
-        +"&units=metric&appid=" 
-        + this.apiKey
-        ).then((response) => response.json())
-        .then((data) => this.displayWeather(data));
-    },
-    displayWeather: function(data) {
-       const { name } = data;
-       const { icon, description } = data.weather[0];
-       const { temp, humidity } = data.main;
-       const { speed } = data.wind;
-       console.log(name,icon,description,temp,humidity,speed);
-       document.querySelector(".city").innerText = "weather in " + name;
-       document.querySelector(".icon").src = "https://api.openweathermap.org/img/wn/" + icon + ".png";
-       document.querySelector(".description").innerText = description;
-       document.querySelector(".temp").innerText = temp + "°C";
-       document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
-       document.querySelector(".wind").innerHTML = "Wind speed: " + speed + " km/h";
-       document.querySelector(".weather").classList.remove("loading");
-       document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
-    },
-    search: function() {
+  apiKey: "1365c68e3f688236f5e083df3a316a78",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "weather in " + name;
+    document.querySelector(".icon").src =
+      "https://api.openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerHTML =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerHTML =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
     this.fetchWeather(document.querySelector(".search-box").value);
-    }
-    };
-    document.querySelector(".search button").addEventListener('click', function() {
-    weather.search();
-   });
+  },
+};
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
 
-   document.querySelector(".search-box").addEventListener("keyup", function(event) {
+document
+  .querySelector(".search-box")
+  .addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
-        weather.search();
+      weather.search();
     }
-   });
-   weather.fetchWeather("Barcelona");
+  });
+weather.fetchWeather("Barcelona");
 
-   // time function part
-   function checkTime(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
+// time function part
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
   }
-  
-  function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    // add a zero in front of numbers<10
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
-    t = setTimeout(function() {
-      startTime()
-    }, 500);
-  }
-  startTime();
+  return i;
+}
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  // add a zero in front of numbers<10
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("time").innerHTML = h + ":" + m + ":" + s;
+  t = setTimeout(function () {
+    startTime();
+  }, 500);
+}
+startTime();
